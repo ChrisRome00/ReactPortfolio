2e916863bc3036c0d19afb4f05e3eda64d18eb60
Christopher Romero <romero.chrisprof@gmail.com>
2024-09-09 15:44:30 -0500

    configuring

--- ReactPortfolio/src/pages/AboutMe.jsx
+++ ReactPortfolio/src/pages/AboutMe.jsx
@@ -1,10 +1,12 @@
 import './styles/AboutMe.css'
+import dogImage from '../../src/assets/images/dog.png';
+
 export default function AboutMe() {
     return (
         <div className='aboutMe'>
             <h2 className='subTitle'>Full-Stack Web Developer</h2>
             <div className="aboutMeBody">
-                <img className='dog' src="./src/assets/images/dog.png" />
+                <img className='dog' src={dogImage} />
                 <p className='aboutMeText'>Hey All! I am a full-stack web devloper based in Austin Texas who loves to create, develop, and learn. Over the past several months, I have been learning under the MERN development stack and have loved every step of the way. Some of my hobbies include playing video games, and When I'm not studying almost 25 hours out of the week, most of all, I like to spend my time sitting in front of an AC unit on full blast, listening to music on my noise canceling headphones, and reading whatever manga has my attention until I inevitbly take a 2 hour nap and mess up my sleep schedule (It's worth it though).</p>
             </div>
         </div>
